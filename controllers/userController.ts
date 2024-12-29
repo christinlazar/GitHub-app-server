ef8a09f462fbb11386522c3fae9f6c9c0d31b45e
christinlazar <christinlazar19@gmail.com>
2024-12-29 21:24:45 +0530

    feat(app):complete project build-backend

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -2,7 +2,7 @@ import {Request,response,Response,NextFunction} from 'express'
 import User from '../models/userModel'
 import { getUserFromGitHub,getFollowers,getFollowing,getMutualFollows, getUserRepositories } from '../services/githubServices'
 
-export const searchUser = async (req: Request, res: Response, next: NextFunction):Promise<any>=> {
+export const searchUser = async (req: Request, res: Response, ):Promise<any>=> {
     const { username } = req.query;
     if (!username) {
       return res.status(400).json({ success: false, message: 'Username is required' });
@@ -46,56 +46,51 @@ export const searchUser = async (req: Request, res: Response, next: NextFunction
         const user_repositories = await getUserRepositories(user.username);
         return res.status(200).json({ success: true, repo_data: user_repositories,user });
       }
-    } catch (error) {
-      next(error);
+    } catch (error:unknown) {
+      return res.status(500).json({message:"internal Server Error",error:error})
     }
   };
 
-  export const blockUser = async (req:Request,res:Response,next:NextFunction):Promise<any> =>{
+  export const blockUser = async (req:Request,res:Response):Promise<any> =>{
       try {
         const {username} = req.body
         const user = await User.findOneAndUpdate({username},{$set:{isBlocked:true}})
         return res.status(200).json({success:true,message:"blocked successfullly",user})
       } catch (error) {
-        console.log(error)
-        next(error)
+        return res.status(500).json({message:"internal Server Error",error:error})
       }
   }
 
-  export const unblockUser = async (req:Request,res:Response,next:NextFunction):Promise<any> =>{
+  export const unblockUser = async (req:Request,res:Response):Promise<any> =>{
     try {
       const {username} = req.body
       const user = await User.findOneAndUpdate({username},{$set:{isBlocked:false}})
       return res.status(200).json({success:true,message:"blocked successfullly",user})
     } catch (error) {
-      console.log(error)
-      next(error)
+      return res.status(500).json({message:"internal Server Error",error:error})
     }
 }
 
-export const editDetails = async (req:Request,res:Response,next:NextFunction):Promise<any> =>{
+export const editDetails = async (req:Request,res:Response):Promise<any> =>{
   try {
     const {formData} = req.body
-    console.log("formData",formData)
     const user = await User.findOneAndUpdate({username:formData.username},{$set:{bio:formData.bio,blog:formData.blog,location:formData.Location}},{new:true})
     return res.status(200).json({success:true,message:"updated successfully",user})
   } catch (error) {
-    console.log(error);
-    next(error)
+    return res.status(500).json({message:"internal Server Error",error:error})
   }
 }
 
-export const getAllUsers = async (req:Request,res:Response,next:NextFunction):Promise<any> =>{
+export const getAllUsers = async (req:Request,res:Response):Promise<any> =>{
   try {
     const users = await User.find({})
     return res.status(200).json({success:true,users})
   } catch (error) {
-    next(error)
+    return res.status(500).json({message:"internal Server Error",error:error})
   }
 }
 
-export const sortUsers = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
-  console.log("req,",req.query)
+export const sortUsers = async (req: Request, res: Response, ): Promise<any> => {
   const { sortBy } = req.query;
   const validSortFields = ['public_repos', 'public_gists', 'followers', 'following', 'createdAt'];
   if (!validSortFields.includes(sortBy as string)) {
@@ -105,10 +100,31 @@ export const sortUsers = async (req: Request, res: Response, next: NextFunction)
     const users = await User.find()
       .sort({ [sortBy as string]: 1 })  
       .exec();
-      console.log("users",users)
     res.json({ users });
   } catch (error) {
-    console.log(error)
-    next(error); 
+    return res.status(500).json({message:"internal Server Error",error:error}) 
   }
 };
+export const seacrhByUser = async (req:Request,res:Response):Promise<any> =>{
+  const { SearchBy } = req.query; 
+  
+  if (!SearchBy) {
+    return res.status(400).json({ message: 'Search term is required' });
+  }
+  try {
+    const query: any = {
+      $or: [
+        { username: { $regex: SearchBy as string, $options: 'i' } }, 
+        { location: { $regex: SearchBy as string, $options: 'i' } }    
+      ]
+    };
+    const users = await User.find(query).exec();
+    if (users.length > 0) {
+      return res.json({ users });
+    } else {
+      return res.status(404).json({ message: 'No users found matching the search term' });
+    }
+  } catch (error) {
+    return res.status(500).json({message:"internal Server Error",error:error})
+  }
+}
--- routes/userRoute.ts
+++ routes/userRoute.ts
@@ -1,11 +1,11 @@
 import express from 'express'
-import {blockUser, editDetails, getAllUsers, searchUser, sortUsers, unblockUser} from '../controllers/userController'
+import {blockUser, editDetails, getAllUsers, seacrhByUser, searchUser, sortUsers, unblockUser} from '../controllers/userController'
 const userRoute = express.Router()
-console.log("in  userRoute")
 userRoute.get('/user',searchUser)
 userRoute.patch('/block-user',blockUser)
 userRoute.patch('/unblock-user',unblockUser)
 userRoute.patch('/edit-details',editDetails)
 userRoute.get('/users',getAllUsers)
 userRoute.get('/sort',sortUsers)
+userRoute.get('/search',seacrhByUser)
 export default userRoute
